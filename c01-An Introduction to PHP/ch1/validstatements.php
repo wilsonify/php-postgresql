229800ebcfc652407a5e013d85ffe2f219071a11
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-25 10:18:36 -0500

    c01-An Introduction to PHP

--- c01-An Introduction to PHP/ch1/validstatements.php
+++
@@ -1,5 +0,0 @@
-<?php
-   $number = "5";               # $number is a string
-   $sum = 15 + $number;   # Add an integer and string to produce integer
-   $sum = "twenty";            # Overwrite $sum with a string.
-?>
---
+++ c01-An Introduction to PHP/src/validstatements.php
@@ -0,0 +1,19 @@
+<?php
+   $number = "5";               # $number is a string
+   echo "number = $number<br>";
+
+   # Add vertical whitespace here
+   echo "<br>";
+
+   $sum = 15 + $number;         # Add an integer and string to produce integer
+   echo "sum (as integer) = $sum<br>";
+
+   # Add vertical whitespace here
+   echo "<br>";
+
+   $sum = "twenty";             # Overwrite $sum with a string.
+   echo "sum (as string) = $sum<br>";
+
+   # Add vertical whitespace here
+   echo "<br>";
+?>
